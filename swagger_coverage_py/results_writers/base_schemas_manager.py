59624dedc7379903a52c84ab35d849a47df1519e
Galina Mavlyutova <78936085+GalinaN-create@users.noreply.github.com>
2025-06-30 22:52:35 +0500

    Update base_schemas_manager.py

--- swagger_coverage_py/results_writers/base_schemas_manager.py
+++ swagger_coverage_py/results_writers/base_schemas_manager.py
@@ -138,7 +138,7 @@ class ApiDocsManagerBase:
             "/", "-"
         ).replace(":", "_")
         path_ = f"swagger-coverage-output/{self.__get_output_subdir()}"
-        pathlib.Path(_path_).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(path_).mkdir(parents=True, exist_ok=True)
         file_path = f"{path_}/{file_name}".split("?")[0]
         file_path = f"{file_path} ({rnd}).{API_DOCS_FORMAT}"
 
